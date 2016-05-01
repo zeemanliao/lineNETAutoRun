fa9c9426ea641bc9b54df00ec3070864a50b1b9c
zeemanliao <zeeman.liao@gmail.com>
2016-05-01 11:05:36 +0800

    update for google cloud setup

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 'use strict';
 let config = require('./config.json');
-let epa = require('./lib/epa')(config);
+
+let cfg = require('../web/config.json');
+let mongoose = require('mongoose');
+let isDEV = process.env.NODE_ENV !== 'production';
+
+if (isDEV) {
+  cfg.db.mongodb.server="localhost";
+}
+
+//mongo db connect
+mongoose.connect('mongodb://' + cfg.db.mongodb.server + '/' + cfg.db.mongodb.db,
+  {
+    user:cfg.db.mongodb.user,
+    pass:cfg.db.mongodb.pass
+  });
+let Storage = require('../web/lib/storage')(mongoose);
+let epa = require('./lib/epa')(config, Storage);
+
+config.db = cfg.db;
 epaRun();
 function epaRun() {
 	epa();
